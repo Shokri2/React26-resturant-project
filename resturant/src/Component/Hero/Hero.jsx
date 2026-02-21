a2b35ae321f5cd5bcb751acbb0e446266f709092
Shokri2 <shokriadnan2005@gmail.com>
2026-02-21 16:16:35 +0300

    generate hero page & about page

---
+++ resturant/src/Component/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+function Hero() {
+    return(
+    <>
+    <div className="container">
+   <img src="./assets" alt="" />
+   <h1>resturant</h1>
+
+    </div>
+    
+    </>);
+} 
+export default Hero;
