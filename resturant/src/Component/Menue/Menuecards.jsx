5ab54c1b5994f6d25135aebc1e0d2f10465227ff
Shokri2 <shokriadnan2005@gmail.com>
2026-02-28 14:23:20 +0300

    add modal

--- resturant/src/Component/Menue/Menuecards.jsx
+++ resturant/src/Component/Menue/Menuecards.jsx
@@ -5,21 +5,67 @@ import {
   Button,
   CardActions,
   CardMedia,
+  Box,
+  Modal,
 } from "@mui/material";
+import { useState } from "react";
 // child component its always child
-function Menuecards({ name, description, image }) {
+function Menuecards({ name, descraption, image, quantity, price }) {
+  const [open, setOpen] = useState(false);
+  const handleopen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" image="{image}" height="200" />
-      <CardContent>
-        <Typography>{name}</Typography>
-        <Typography>{description}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">add to cart</Button>
-        <Button size="small">view</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" image={image} height="182" />
+        <CardContent>
+          <Typography>{name}</Typography>
+          <Typography>{descraption}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={handleopen}>
+            add to cart
+          </Button>
+          <Button size="small">view</Button>
+        </CardActions>
+      </Card>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            width: 400,
+            margin: "400px",
+            padding: "70px",
+            bgcolor: "white",
+            border: "2px solid #000",
+            boxShadow: 24,
+            borderRadius: "20px",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h6" align="center">
+            {name} add to cart! <br />
+          </Typography>
+          <Typography variant="body1" align="center">
+            price: {price}$
+          </Typography>
+          <Typography variant="body1" align="center">
+            description: {descraption}
+          </Typography>
+
+          <Typography variant="body1" align="center">
+            <Button>+</Button> quantity: {quantity} <Button>-</Button>
+          </Typography>
+          <Button sx={{ textAlign: "center" }} onClick={handleClose}>
+            {" "}
+            Done!
+          </Button>
+        </Box>
+      </Modal>
+    </>
   );
 }
 export default Menuecards;
--- resturant/src/Component/Menue/Menuelist.jsx
+++ resturant/src/Component/Menue/Menuelist.jsx
@@ -1,38 +1,50 @@
 import { Grid, Typography, Container } from "@mui/material";
 import Menuecards from "./Menuecards.jsx";
+
 // parent component
 // id => uniqe value
 function Menuelist() {
   const piza = [
     {
-      id: 1,    
-      name: "dd",
-      descraption: "sssss",
-      image: "",
+      id: 1,
+      name: "Gold Steak",
+      descraption:
+        "A luxurious pie",
+      image: "./src/assets/image11.png",
+      price: 10.9,
+      quantity: 12,
     },
     {
       id: 2,
       name: "dd",
       descraption: "sssss",
-      image: "",
+      image: "./src/assets/image88.png",
+      price: 10.9,
+      quantity: 12,
     },
     {
       id: 3,
       name: "dd",
       descraption: "sssss",
-      image: "",
+      image: "./src/assets/image88.png",
+      price: 10.9,
+      quantity: 12,
     },
     {
       id: 4,
       name: "dd",
       descraption: "sssss",
       image: "./src/assets",
+      price: 10.9,
+      quantity: 12,
     },
     {
       id: 5,
       name: "dd",
       descraption: "sssss",
       image: "",
+      price: 10.9,
+      quantity: 12,
     },
   ];
   return (
@@ -43,9 +55,17 @@ function Menuelist() {
         </Typography>
         <Grid container spacing={12}>
           {piza.map((pizza) => {
-           return( <Grid item xs={12} sm={6} md={5} key={pizza.id}>
-              <Menuecards name={pizza.name} descraption={pizza.descraption} image={pizza.image}/>
-            </Grid>);
+            return (
+              <Grid item xs={12} sm={6} md={5} key={pizza.id}>
+                <Menuecards
+                  name={pizza.name}
+                  descraption={pizza.descraption}
+                  image={pizza.image}
+                  price={pizza.price}
+                  quantity={pizza.quantity}
+                />
+              </Grid>
+            );
           })}
         </Grid>
       </Container>
