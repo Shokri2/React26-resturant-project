5aba8f99e426fe94cd48d23e194a30f440232ca1
Shokri2 <shokriadnan2005@gmail.com>
2026-03-02 21:28:19 +0300

    add event add onClick on view button
    handle view logic
    
    install react router  dom
    
    props drilling between three component
    
    define routes using browser routes

--- resturant/src/App.jsx
+++ resturant/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import LandingPage from "./Component/pages/LandingPage/landingpage.jsx";
-
+import { Route, Routes } from "react-router-dom";
+import Viewitem from "./Component/Menue/Viewitem.jsx";
 function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/view-details" element={<Viewitem />} />
+      </Routes>
     </>
   );
 }
